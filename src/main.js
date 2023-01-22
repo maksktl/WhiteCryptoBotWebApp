37f3234067e918f6405f9475a99150512cc4a3ed
e-maks <matantsev0@gmail.com>
2023-01-22 04:24:07 +0300

    Remove typo

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
-window.Telegram.WebApp
+
 
 const app = createApp(App)
 app.use(Antd) // same as Vue.use in Vue 2.X
